62c7c11ade48a7a1a9422aa3f8789d9311bd87fa
Pablo <pablobaiz12@gmail.com>
2021-05-15 17:11:37 -0300

    end first part

--- index.js
+++ index.js
@@ -14,30 +14,72 @@ const Direction = {
 };
 let controls = {
   directions: { x: 1, y: 0 },
-  snake: [{ x: 50, y: 50 }],
-  point: { x: 20, y: 50 },
+  snake: [{ x: 0, y: 50 }],
+  point: { x: 0, y: 250 },
   growth: 0,
   play: false,
 };
 
 const move = function () {
+  const bodySnake = {};
   const init = controls.snake[0];
+  // copy last elemnt in init snake
+  Object.assign(bodySnake, controls.snake[controls.snake.length - 1]);
+
+  // agree skake point
   const food = init.x === controls.point.x && init.y === controls.point.y;
+
+  // init new game
+  if (lostGame()) {
+    controls.play = true;
+    newGame();
+  }
+
+  // body snake
   const controlX = controls.directions.x;
   const controlY = controls.directions.y;
-  init.x += controlX;
-  init.y += controlY;
 
   const size = controls.snake.length - 1;
-
-  for (let i = size; size < size - 1; size--)
-    if (food) {
-      eating();
+  if (controls.play) {
+    for (let idx = size; idx > -1; idx--) {
+      const init = controls.snake[idx];
+      if (idx === 0) {
+        init.x += controlX;
+        init.y += controlY;
+      } else {
+        init.x = controls.snake[idx - 1].x;
+        init.y = controls.snake[idx - 1].y;
+      }
     }
+  }
+
+  if (food) {
+    controls.growth += 10;
+    eating();
+  }
+
+  if (controls.growth > 0) {
+    controls.snake.push(bodySnake);
+    controls.growth -= 1;
+  }
 
   requestAnimationFrame(snakeColor);
 
   setTimeout(move, 80);
+};
+
+// compare whether the shock of the wall of the snake or against itself
+const lostGame = function () {
+  const init = controls.snake[0];
+  if (init.x < 0 || init.x >= 500 / 5 || init.y < 0 || init.y >= 500 / 5) {
+    return true;
+  }
+  for (let idx = 1; idx < controls.snake.length; idx++) {
+    const bodySnake = controls.snake[idx];
+    if (bodySnake.x === init.x && bodySnake.y === init.y) {
+      return true;
+    }
+  }
 };
 
 document.onkeydown = (e) => {
@@ -52,15 +94,19 @@ document.onkeydown = (e) => {
 
 const snakeColor = function () {
   context.clearRect(0, 0, 500, 500);
-  const init = controls.snake[0];
+  for (let idx = 0; idx < controls.snake.length; idx++) {
+    const { x, y } = controls.snake[idx];
+    players('green', x, y);
+  }
+
   const point = controls.point;
-  players('green', init.x, init.y);
+
   players('white', point.x, point.y);
 };
 
 const players = function (color, x, y) {
   context.fillStyle = color; //color snake
-  context.fillRect(x * 5, y * 5, 10, 10); // position x, y and width, heigth
+  context.fillRect(x * 8, y * 8, 5, 5); // position x, y and width, heigth
 };
 
 const eating = function () {
@@ -70,16 +116,24 @@ const eating = function () {
   food.y = dam.y;
 };
 
+// this sum one px in bodySnake
 const newPoint = function () {
   const directionSnake = Object.values(Direction);
   return {
-    x: parseInt((Math.random() * 500) / 5),
-    y: parseInt((Math.random() * 500) / 5),
-    j: directionSnake[parseInt(Math.random() * 3)],
+    x: parseInt((Math.random() * 500) / 8),
+    y: parseInt((Math.random() * 500) / 8),
+    j: directionSnake[parseInt(Math.random() * 7)],
   };
 };
 
-window.onload = function () {
+const newGame = function () {
+  controls = {
+    directions: { x: 1, y: 0 },
+    snake: [{ x: 0, y: 50 }],
+    point: { x: 0, y: 250 },
+    growth: 0,
+    play: false,
+  };
   const position = newPoint();
   const snake = controls.snake[0];
   snake.x = position.x;
@@ -90,6 +144,10 @@ window.onload = function () {
   const food = controls.point;
   food.x = dam.x;
   food.y = dam.y;
+  controls.play = true;
+};
 
+window.onload = function () {
+  newGame();
   move();
 };
